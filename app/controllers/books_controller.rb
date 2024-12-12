9e4d08eaa8be4db06226e900aa3edbe98300ce27
NaokiHigashida <higekoikai09@gmail.com>
2024-12-12 12:30:11 +0000

    [Finish] Bookersを作成しよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
 
 
@@ -12,6 +11,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
