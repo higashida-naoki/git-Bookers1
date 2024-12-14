ad251bc4aee00daeed9258709c5f14b031bb5dd9
NaokiHigashida <188346346+higashida-naoki@users.noreply.github.com>
2024-12-14 12:26:22 +0000

    [Finish] Bookersを作成しよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created"
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
@@ -25,9 +27,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
